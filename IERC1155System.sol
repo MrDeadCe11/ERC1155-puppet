e70b90a764da73ff4869932a04ed54506969f797
MrDeadCe11 <mister.deadce11@gmail.com>
2024-06-17 23:50:48 -0500

    update from codegen

--- IERC1155System.sol
+++ IERC1155System.sol
@@ -9,51 +9,47 @@ pragma solidity >=0.8.24;
  * @dev This interface is automatically generated from the corresponding system contract. Do not edit manually.
  */
 interface IERC1155System {
-  function setApprovalForAll(address operator, bool approved) external;
+    function setApprovalForAll(address operator, bool approved) external;
 
-  function isApprovedForAll(address owner, address operator) external view returns (bool);
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
 
-  function transferFrom(address from, address to, uint256 tokenId, uint256 value) external;
+    function transferFrom(address from, address to, uint256 tokenId, uint256 value) external;
 
-  function safeTransferFrom(address from, address to, uint256 tokenId, uint256 value) external;
+    function safeTransferFrom(address from, address to, uint256 tokenId, uint256 value) external;
 
-  function safeTransferFrom(address from, address to, uint256 tokenId, uint256 value, bytes memory data) external;
+    function safeTransferFrom(address from, address to, uint256 tokenId, uint256 value, bytes memory data) external;
 
-  function safeBatchTransferFrom(
-    address from,
-    address to,
-    uint256[] calldata ids,
-    uint256[] calldata values,
-    bytes calldata data
-  ) external;
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] calldata ids,
+        uint256[] calldata values,
+        bytes calldata data
+    ) external;
 
-  function mint(address to, uint256 tokenId, uint256 value) external;
+    // function mint(address to, uint256 tokenId, uint256 value) external;
 
-  function mint(address to, uint256 tokenId, uint256 value, bytes memory data) external;
+    function mint(address to, uint256 tokenId, uint256 value, bytes memory data) external;
 
-  function safeMint(address to, uint256 tokenId, uint256 value) external;
+    function safeMint(address to, uint256 tokenId, uint256 value) external;
 
-  function safeMint(address to, uint256 tokenId, uint256 value, bytes memory data) external;
+    function safeMint(address to, uint256 tokenId, uint256 value, bytes memory data) external;
 
-  function burn(uint256 tokenId, uint256 value) external;
+    function burn(uint256 tokenId, uint256 value) external;
 
-  function balanceOf(address owner, uint256 id) external view returns (uint256);
+    function balanceOf(address owner, uint256 id) external view returns (uint256);
 
-  function balanceOfBatch(address[] memory accounts, uint256[] memory ids) external view returns (uint256[] memory);
+    function balanceOfBatch(address[] memory accounts, uint256[] memory ids) external view returns (uint256[] memory);
 
-  function uri(uint256 tokenId) external view returns (string memory);
+    function uri(uint256 tokenId) external view returns (string memory);
 
-  function setTokenURI(uint256 tokenId, string memory tokenURI) external;
+    function setTokenURI(uint256 tokenId, string memory tokenURI) external;
 
-  function totalSupply(uint256 tokenId) external view returns (uint256 _totalSupply);
+    function totalSupply(uint256 tokenId) external view returns (uint256 _totalSupply);
 
-  function onERC1155Received(address, address, uint256, uint256, bytes calldata) external returns (bytes4 retval);
+    function onERC1155Received(address, address, uint256, uint256, bytes calldata) external returns (bytes4 retval);
 
-  function onERC1155BatchReceived(
-    address,
-    address,
-    uint256[] calldata,
-    uint256[] calldata,
-    bytes calldata
-  ) external returns (bytes4 retval);
+    function onERC1155BatchReceived(address, address, uint256[] calldata, uint256[] calldata, bytes calldata)
+        external
+        returns (bytes4 retval);
 }
